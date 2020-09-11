21d03431c52c8a522e6fb2c7e5c673608188f568
ILYVET <ivetchinin@gmail.com>
2020-09-11 19:10:08 +0300

    tests

--- node.py
+++ node.py
@@ -35,8 +35,6 @@ class Node:
             raise TypeError
         self.__prev = prev
 
-    def __str__(self):
-        return f"{repr(self.__prev)} >> Value: {self.value} >> {repr(self.__next)}"
 
     def __repr__(self):
         return f"{self.__class__.__name__}({self.value}, {self.__next}, {self.__prev})"
